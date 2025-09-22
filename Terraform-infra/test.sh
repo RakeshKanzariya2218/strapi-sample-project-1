3a5ff657a7a21d25401396dde479700278bc55a0
Rakeshkanzariya2218 <rkanzariya1234@gmail.com>
2025-09-22 12:34:02 +0530

    final

--- Terraform-infra/test.sh
+++ Terraform-infra/test.sh
@@ -6,13 +6,8 @@ yum install aws-cli -y
 systemctl start docker
 systemctl enable docker
 sleep 5
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 132866222051.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 132866222051.dkr.ecr.us-east-1.amazonaws.co
 docker volume create strapi-data
-docker pull 132866222051.dkr.ecr.us-east-1.amazonaws.com/strapi/strapi:latest
-docker run --rm -v strapi-data:/srv/app 132866222051.dkr.ecr.us-east-1.amazonaws.com/strapi/strapi:latest strapi new /srv/app --quickstart
-docker run -dt -p 80:1337 -v strapi-data:/srv/app --name strapi 132866222051.dkr.ecr.us-east-1.amazonaws.com/strapi/strapi:latest
-
-
-
-
-
+docker pull 132866222051.dkr.ecr.us-east-1.amazonaws.com/strapi/strapi:${docker_image_tag}
+docker run --rm -v strapi-data:/srv/app 132866222051.dkr.ecr.us-east-1.amazonaws.com/strapi/strapi:${docker_image_tag} strapi new /srv/app --quickstart
+docker run -dt -p 80:1337 -v strapi-data:/srv/app --name strapi 132866222051.dkr.ecr.us-east-1.amazonaws.com/strapi/strapi:${docker_image_tag}
