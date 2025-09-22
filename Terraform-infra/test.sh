66d1ba10fd8db8eff0a5846a2717910385df3711
Rakeshkanzariya2218 <rkanzariya1234@gmail.com>
2025-09-22 17:31:07 +0530

    final

--- Terraform-infra/test.sh
+++ Terraform-infra/test.sh
@@ -11,3 +11,15 @@ docker volume create strapi-data
 docker pull 132866222051.dkr.ecr.us-east-1.amazonaws.com/strapi/strapi:${docker_image_tag}
 docker run --rm -v strapi-data:/srv/app 132866222051.dkr.ecr.us-east-1.amazonaws.com/strapi/strapi:${docker_image_tag} strapi new /srv/app --quickstart
 docker run -dt -p 1337:1337 -v strapi-data:/srv/app --name strapi 132866222051.dkr.ecr.us-east-1.amazonaws.com/strapi/strapi:${docker_image_tag}
+docker run -d \
+  -p 1337:1337 \
+  -v strapi-data:/srv/app \
+  -e APP_KEYS="$(openssl rand -hex 16),$(openssl rand -hex 16),$(openssl rand -hex 16),$(openssl rand -hex 16)" \
+  -e ADMIN_JWT_SECRET="$(openssl rand -hex 16)" \
+  -e API_TOKEN_SALT="$(openssl rand -hex 16)" \
+  -e TRANSFER_TOKEN_SALT="$(openssl rand -hex 16)" \
+  -e ENCRYPTION_KEY="$(openssl rand -hex 32)" \
+  -e FLAG_NPS=true \
+  -e FLAG_PROMOTE_EE=true \
+  --name strapi \
+  132866222051.dkr.ecr.us-east-1.amazonaws.com/strapi/strapi:728eb5e9
