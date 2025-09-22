7238336587fc87b1efabf27668bccc847507416d
Rakeshkanzariya2218 <rkanzariya1234@gmail.com>
2025-09-22 17:54:18 +0530

    final

--- rakesh-strapi-project/config/admin.ts
+++ rakesh-strapi-project/config/admin.ts
@@ -1,17 +1,26 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'default_admin_jwt_secret'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env(
+      'API_TOKEN_SALT',
+      'default_api_token_salt_' + Math.random().toString(36).slice(2)
+    ),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env(
+        'TRANSFER_TOKEN_SALT',
+        'default_transfer_token_salt_' + Math.random().toString(36).slice(2)
+      ),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env(
+      'ENCRYPTION_KEY',
+      'default_encryption_key_' + Math.random().toString(36).repeat(4)
+    ),
   },
   flags: {
     nps: env.bool('FLAG_NPS', true),
