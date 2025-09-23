e640de5f63b52f81d1c889f8ee36a85a6cfaf935
Rakeshkanzariya2218 <rkanzariya1234@gmail.com>
2025-09-23 09:01:12 +0530

    final-action

--- Terraform-infra/variables.tf
+++ Terraform-infra/variables.tf
@@ -28,17 +28,6 @@ variable "docker_image_tag" {
   type        = string
 }
 
-variable "aws_access_key" {
-  description = "AWS Access Key ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  sensitive   = true
-}
 
 
 
